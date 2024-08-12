301311bda65ad9ea100b42739250a5ae103ecb25
AnuragM23 <anuragmahato006@gmail.com>
2024-08-12 22:35:30 +0530

    added the skeleton of the web app

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+import logo from "../assets/logo.svg"
+import Navbar from './Navbar';
+
+function Header() {
+  return (
+    <header className='fixed top-0 left-0 m-auto max-padd_container w-full bg-white ring-1 ring-slate-900/5 z-10'>
+        <div className='px-4 flexBetween py-3 max-xs:px-2'>
+            {/* logo */}
+            <div>
+                
+                <Link><img src={logo} alt="" height={66} width={66} /></Link>
+            </div>
+            {/* navbar */}
+            <Navbar />
+
+            {/* buttons */}
+            <div>buttons</div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
