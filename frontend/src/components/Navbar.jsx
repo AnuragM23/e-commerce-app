70c6f2260df4fe8bae596a890e5ce3a2adad18fe
AnuragM23 <anuragmahato006@gmail.com>
2024-08-13 13:27:36 +0530

    navbar created and fully responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {MdCategory, MdContacts, MdHomeFilled, MdShop2} from 'react-icons/md'
 
-function Navbar() {
+function Navbar({containerStyles}) {
   return (
-    <nav >
-
+    <nav className={`${containerStyles}`}>
+      <NavLink to={'/'}><div className='flexCenter gap-x-1'><MdHomeFilled/>Home</div></NavLink>
+      <NavLink to={'/mens'}><div className='flexCenter gap-x-1'><MdCategory/>Men's</div></NavLink>
+      <NavLink to={'/womens'}><div className='flexCenter gap-x-1'><MdShop2/>Women's</div></NavLink>
+      <NavLink to={'/kids'}><div className='flexCenter gap-x-1'><MdContacts/>Kid's</div></NavLink>
     </nav>
   )
 }
