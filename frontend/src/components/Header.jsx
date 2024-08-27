1b397c2175227089e129f6e6b5786f48628677aa
AnuragM23 <anuragmahato006@gmail.com>
2024-08-27 14:43:59 +0530

    completed the major-frontend part

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {Link, NavLink} from 'react-router-dom';
 import logo from "../assets/logo.svg"
 import logout from "../assets/logout.svg"
@@ -6,13 +6,14 @@ import user from "../assets/user.svg"
 import Navbar from './Navbar';
 import { MdClose, MdMenu } from 'react-icons/md';
 import {FaOpencart} from 'react-icons/fa';
+import { ShopContext } from '../contexts/ShopContext';
 
 function Header() {
 
   const [menuOpened, setMenuOpened] = useState(false);
 
   const toggleMenu = () => setMenuOpened(!menuOpened);
-
+  const {getTotalCartItems} = useContext(ShopContext);
 
   return (
     <header className='fixed top-0 left-0 m-auto max-padd_container w-full bg-white ring-1 ring-slate-900/5 z-10'>
@@ -32,7 +33,7 @@ function Header() {
             <div className='flexBetween sm:gap-x-6 bold-16'>
               {!menuOpened?(<MdMenu className='md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-rounded-full w-[40px] hover:ring-secondary' onClick={toggleMenu}/>):(<MdClose className='md:hidden cursor-pointer hover:text-secondary mr-2 p-1 ring-1 ring-slate-900/30 h-8 w-rounded-full w-[40px] hover:ring-secondary' onClick={toggleMenu}/>)}
               <div className="flexBetween sm:gap-x-3">
-                <NavLink to={"cart-page"} className={"flex"}><FaOpencart className='p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full'/> <span className='relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2'>0</span> </NavLink>
+                <NavLink to={"cart-page"} className={"flex"}><FaOpencart className='p-1 h-8 w-8 ring-slate-900/30 ring-1 rounded-full'/> <span className='relative flexCenter w-5 h-5 rounded-full bg-secondary text-white medium-14 -top-2'>{getTotalCartItems()}</span> </NavLink>
               </div>
               {/* <NavLink to={'logout'} className={"btn_secondary_rounded flexCenter gap-x-2 medium-14"}><img src={logout} alt='logoutIcon' height={19} width={19}/>Logout</NavLink> */}
               <NavLink to={'login'} className={"btn_secondary_rounded flexCenter gap-x-2 medium-14"}><img src={user} alt='userIcon' height={19} width={19}/>Login</NavLink>
--- frontend/src/contexts/ShopContext.jsx
+++ frontend/src/contexts/ShopContext.jsx
@@ -3,34 +3,62 @@ import all_products from "../assets/all_products";
 
 export const ShopContext = createContext(null);
 const getDefaultCart = () => {
-        let cart = {};
-        for (let i = 0; i < all_products.length + 1; i++) {
-            cart[i] = 0;
-        }
-        return cart;
-    };
-const ShopContextProvider = (props) => {   
-
-    const [cartItems, setCartItems] = useState(getDefaultCart());
-    
-    
-    const addToCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}));
-    }
-
-    const removeToCart = (itemId) => {
-        setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}));
-    }
-    
-    const contextValue = {all_products, cartItems, addToCart, removeToCart};
-
-    return (
-        <ShopContext.Provider value={contextValue}>
-            {props.children}
-        </ShopContext.Provider>
-    )
-
+  let cart = {};
+  for (let i = 0; i < all_products.length + 1; i++) {
+    cart[i] = 0;
+  }
+  return cart;
 };
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
 
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    // console.log(cartItems);
+  };
+
+  const removeToCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_products.find((p) => p.id === Number(item));
+        totalAmount += itemInfo.new_price * Number(cartItems[item]);
+      }
+    }
+    // console.log(totalAmount);
+    
+    return totalAmount;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for(let item in cartItems) {
+        if (cartItems[item] > 0) {
+            totalItem += cartItems[item];
+        }
+    }
+
+    return totalItem;
+  }
+
+  const contextValue = {
+    getTotalCartItems, 
+    getTotalCartAmount,
+    all_products,
+    cartItems,
+    addToCart,
+    removeToCart,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
+};
 
 export default ShopContextProvider;
